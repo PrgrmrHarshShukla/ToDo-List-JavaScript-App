2ebb48ef233103a8f5c86bb7121989e6ed9c7ff6
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-05-20 21:45:08 +0530

    Initial commit

---
+++ ui/script.js
@@ -0,0 +1,84 @@
+// TODO LIST APP
+
+// // console.log(localStorage.getItem('todo'));
+
+
+// // const indexer = () => {
+// //    for (let i=1, )
+// // }
+
+
+// document.getElementById("bt1").addEventListener('click', (e) => {
+//    e.preventDefault()
+//    let value = input.value
+
+
+//    let todos;
+//    if( window.localStorage.getItem('todo') == null ) {
+//       todos = []
+//    }
+//    else {
+//       todos = JSON.parse(window.localStorage.getItem('todo'))
+//    }
+//    console.log(todos);
+//    window.localStorage.setItem('todo', JSON.stringify([...todos, value]))
+   
+   
+//    input.value = ""   
+// })  
+
+// const func1 = async () => {
+//    let el = document.getElementById("dc2")
+//       let matter = window.localStorage.getItem('todo')
+//       let display = await JSON.parse(matter)
+//       let displayEnd = display[display.length - 1]
+//       console.log(displayEnd);
+//       // console.log(display);
+//       // console.log(matter);
+//       // console.log("display " ,typeof display);
+//       // console.log(typeof todos);
+//       // console.log(typeof display);
+//       // console.log(typeof localStorage.getItem('todo'));
+//       // console.log(localStorage.getItem('todo').length);
+//       el.appendChild(`
+//          <div class="cd" style="
+//             display: flex;
+//             flex-direction: row;
+//             justify-content: space-between;
+//             align-items: center;
+//             gap: 3vh;
+//             border: 2px red solid;
+//          ">
+//             <h2 style="
+//                max-width: 400px;
+//                text-align: center;
+//             ">${displayEnd}</h2>
+//             <button style="
+//             height: 20px; 
+//             border: 1px black solid; 
+//             border-radius: 50px; 
+//             padding: 5px;
+//             padding-top: 0.8px;
+//             font-weight: 600;
+//             background-color: red;
+//             ">x</button>
+//          </div>
+//       `)
+// }
+
+
+
+
+
+
+
+// // document.getElementById("btn2").addEventListener('click', (e) => {
+// //    e.preventDefault()
+// //    let el = document.getElementById("dc2")
+// //    el.innerHTML = null
+// //    window.localStorage.removeItem("todo");
+// // })
+
+
+// // console.log(localStorage.getItem('todo'))
+
