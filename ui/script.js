cad96931ea87ce0bd62dd83db1d6b9018ca9cfa5
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-05-20 23:37:18 +0530

    Design modifications.

--- ui/script.js
+++ ui/script.js
@@ -1,84 +1,98 @@
 // TODO LIST APP
 
-// // console.log(localStorage.getItem('todo'));
+// console.log(localStorage.getItem('todo'));
 
 
-// // const indexer = () => {
-// //    for (let i=1, )
-// // }
-
-
-// document.getElementById("bt1").addEventListener('click', (e) => {
-//    e.preventDefault()
-//    let value = input.value
-
-
-//    let todos;
-//    if( window.localStorage.getItem('todo') == null ) {
-//       todos = []
-//    }
-//    else {
-//       todos = JSON.parse(window.localStorage.getItem('todo'))
-//    }
-//    console.log(todos);
-//    window.localStorage.setItem('todo', JSON.stringify([...todos, value]))
+const fun = () => {
+   bt1.addEventListener('click', (e) => {
+      e.preventDefault()
+      let value = input.value
+      if(value == ""){
+         return
+      }
    
-   
-//    input.value = ""   
-// })  
-
-// const func1 = async () => {
-//    let el = document.getElementById("dc2")
-//       let matter = window.localStorage.getItem('todo')
-//       let display = await JSON.parse(matter)
-//       let displayEnd = display[display.length - 1]
-//       console.log(displayEnd);
-//       // console.log(display);
-//       // console.log(matter);
-//       // console.log("display " ,typeof display);
-//       // console.log(typeof todos);
-//       // console.log(typeof display);
-//       // console.log(typeof localStorage.getItem('todo'));
-//       // console.log(localStorage.getItem('todo').length);
-//       el.appendChild(`
-//          <div class="cd" style="
-//             display: flex;
-//             flex-direction: row;
-//             justify-content: space-between;
-//             align-items: center;
-//             gap: 3vh;
-//             border: 2px red solid;
-//          ">
-//             <h2 style="
-//                max-width: 400px;
-//                text-align: center;
-//             ">${displayEnd}</h2>
-//             <button style="
-//             height: 20px; 
-//             border: 1px black solid; 
-//             border-radius: 50px; 
-//             padding: 5px;
-//             padding-top: 0.8px;
-//             font-weight: 600;
-//             background-color: red;
-//             ">x</button>
-//          </div>
-//       `)
-// }
+      let todos;
+      if( window.localStorage.getItem('todo') == null ) {
+         todos = []
+      }
+      else {
+         todos = JSON.parse(window.localStorage.getItem('todo'))
+      }
+      console.log(todos);
+      window.localStorage.setItem('todo', JSON.stringify([...todos, value]))
+      
+      func1()
+      input.value = ""   
+   })  
+}
+fun()
 
 
 
+const func1 = () => {
+
+   console.log(window);
+
+   let el = document.getElementById("dc2")
+      let matter = window.localStorage.getItem('todo')
+      let display = JSON.parse(matter)
+      let displayEnd = display[display.length - 1]
+      console.log(displayEnd);
+      console.log(display);
+      // console.log(matter);
+      // console.log("display " ,typeof display);
+      // console.log(typeof todos);
+      // console.log(typeof display);
+      // console.log(typeof localStorage.getItem('todo'));
+      // console.log(localStorage.getItem('todo').length);
+      
+         let newEl = document.createElement("h3")
+         newEl.innerHTML = displayEnd
+         newEl.style.border = "1px black solid"
+         newEl.style.padding = "10px"
+         newEl.style.borderRadius = "10px"
+         
+         el.insertAdjacentElement('afterbegin', newEl) 
+}
+
+window.onload = function () {
+   let el = document.getElementById("dc2")
+      let matter = window.localStorage.getItem('todo')
+      let display = JSON.parse(matter)
+      let displayEnd = display[display.length - 1]
+      console.log(displayEnd);
+      console.log(display);
+      // console.log(matter);
+      // console.log("display " ,typeof display);
+      // console.log(typeof todos);
+      // console.log(typeof display);
+      // console.log(typeof localStorage.getItem('todo'));
+      // console.log(localStorage.getItem('todo').length);
+      
+      display.forEach(element => {
+            let newEl = document.createElement("h3")
+            newEl.innerHTML = element
+            newEl.style.border = "1px black solid"
+            newEl.style.padding = "10px"
+            newEl.style.borderRadius = "10px"
+            
+            el.insertAdjacentElement('afterbegin', newEl) 
+         });
+}
 
 
 
 
-// // document.getElementById("btn2").addEventListener('click', (e) => {
-// //    e.preventDefault()
-// //    let el = document.getElementById("dc2")
-// //    el.innerHTML = null
-// //    window.localStorage.removeItem("todo");
-// // })
 
 
-// // console.log(localStorage.getItem('todo'))
+bt2.addEventListener("click", (e) => {
+   e.preventDefault()
+   // let el = document.getElementById("dc2")
+   // el.innerHTML = null
+   window.localStorage.removeItem("todo");
+   window.location.reload()
+})
+
+
+// console.log(localStorage.getItem('todo'))
 
